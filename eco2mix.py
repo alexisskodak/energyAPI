08d29434a73579898284ed05ebcf85e979f26591
alexis <skodak95alexis@gmail.com>
2021-04-15 11:08:52 +0200

    Refactor to improve performance

--- eco2mix.py
+++ eco2mix.py
@@ -1,11 +1,7 @@
 import pandas
 import json
 
-pandas.set_option('max_columns', None)
 df = pandas.read_csv('data/eco2mix_min.csv', sep=';')
-
-# keep only the dates and the CO2 emission rate
-df.drop(df.columns.difference(['Date', 'Taux de CO2 (g/kWh)']), 1, inplace=True)
 
 # drop all rows with NaN values
 df.dropna(subset=['Taux de CO2 (g/kWh)'], inplace=True)
@@ -14,11 +10,22 @@ df.dropna(subset=['Taux de CO2 (g/kWh)'], inplace=True)
 df['Date'] = pandas.to_datetime(df['Date'], format='%Y-%m-%d')
 
 # group by year-month and add sum, mean, max and min columns
-s2 = df.groupby([df['Date'].dt.year, df['Date'].dt.month])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min'])
+# s2 = df.groupby([df['Date'].dt.year, df['Date'].dt.month])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min'])
 
 # group by year
-s3 = df.groupby([df['Date'].dt.year])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min'])
+# s3 = df.groupby([df['Date'].dt.year])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min'])
 
-# Convert to json
-emissions = json.loads(s2.to_json(orient='index'))
-yearly_emissions = json.loads(s3.to_json(orient='index'))
+# Group by year-month and add sum, mean, max and min columns and convert to json
+emissions = json.loads(
+    df.groupby([
+        df['Date'].dt.year,
+        df['Date'].dt.month
+    ])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min']).to_json(orient='index')
+)
+
+# group by year and convert to JSON
+yearly_emissions = json.loads(
+    df.groupby([
+        df['Date'].dt.year
+    ])['Taux de CO2 (g/kWh)'].agg(['sum', 'mean', 'max', 'min']).to_json(orient='index')
+)
